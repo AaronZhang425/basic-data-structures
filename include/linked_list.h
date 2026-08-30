aa92281a06663f71f2cb190aea7bacefcd833df9
Aaron Zhang <aaronbxzhang@gmail.com>
2026-08-30 17:22:31 -0400

    Add linked list functions to header file

--- include/linked_list.h
+++ include/linked_list.h
@@ -11,4 +11,22 @@ struct linked_list {
     uint16_t size;
 };
 
+struct linked_list_node *get_last_node(struct linked_list *linked_list);
+
+int new_link_list(struct linked_list **linked_list);
+
+void destory_linked_list(struct linked_list *linked_list);
+
+void destroy_linked_list_node_full(struct linked_list_node *node);
+
+int linked_list_append(
+    struct linked_list *linked_list,
+    void *data,
+    size_t data_size
+);
+
+void *linked_list_get(struct linked_list *linked_list, uint32_t target_index);
+
+void linked_list_remove(struct linked_list *linked_list, uint32_t target_index);
+
 #endif
